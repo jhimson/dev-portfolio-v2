6c40dcb8ab88390bd18e25abcc9e4aa037e2dda0
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-08-20 17:50:46 -0400

    Updated design for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <main>
+    <main className='h-screen bg-black'>
       <Navbar />
     </main>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="w-full bg-[#191919] text-white p-8 flex justify-between items-center">
-      
+    <div className="w-full text-white p-8 flex justify-between items-center bg-slate-900">
+
       <div className="left-nav-items">
-        <ul className="flex items-end space-x-8">
-          <li className="font-bold text-2xl font-righteous tracking-widest">
+        <ul className="flex items-center space-x-8">
+          <li className="font-bold text-2xl font-righteous tracking-widest hover:bg-slate-800 px-4 py-2 cursor-pointer rounded text-white">
             Jhimson Pamisa
           </li>
-          <li className="font-righteous text-lg tracking-widest">
+          <li className="font-righteous text-lg tracking-widest hover:bg-slate-800 px-4 py-2 cursor-pointer rounded text-gray-400 hover:text-white">
             Tech Skills
           </li>
-          <li className="font-righteous text-lg tracking-widest">Projects</li>
+          <li className="font-righteous text-lg tracking-widest hover:bg-slate-800 px-4 py-2 cursor-pointer rounded text-gray-400 hover:text-white">Projects</li>
         </ul>
       </div>
 
       <div className="right-nav-items">
         <ul className="flex items-end space-x-8">
-          <li className="font-righteous text-lg tracking-widest">Resume</li>
-          <li className="font-righteous text-lg tracking-widest">Contact</li>
+          <li className="font-righteous text-lg tracking-widest hover:bg-slate-800 hover:text-white px-4 py-2 cursor-pointer rounded text-gray-400">Resume</li>
+          <li className="font-righteous text-lg tracking-widest hover:bg-slate-800 hover:text-white px-4 py-2 cursor-pointer rounded text-gray-400">Contact</li>
         </ul>
       </div>
     </div>
